e083a797e9ed0785ca98de34f327474029dab352
Janisson <janisson.bass@gmail.com>
2022-04-21 18:26:10 -0300

    -a

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,11 @@ snake[0] = {
 }
 let direction = "right";
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0,0,16*box,16*box)
@@ -20,15 +25,19 @@ function criarSnake(){
     }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x,food.y,box,box)
+}
+
 document.addEventListener('keydown',update)
 
 function update(event){
     if(event.keyCode === 37 && direction !== 'right') direction = 'left';
-    if(event.keyCode === 38 && direction !== 'up') direction = 'down';
+    if(event.keyCode === 38 && direction !== 'down') direction = 'up';
     if(event.keyCode === 39 && direction !== 'left') direction = 'right';
-    if(event.keyCode === 40 && direction !== 'down') direction = 'up';
+    if(event.keyCode === 40 && direction !== 'up') direction = 'down';
 }
-
 
 function iniciarJogo(){
 
@@ -39,6 +48,7 @@ function iniciarJogo(){
 
     criarBG();
     criarSnake();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
