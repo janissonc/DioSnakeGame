0445816aa81913d7b39595680355be351af54b73
Janisson <janisson.bass@gmail.com>
2022-04-21 18:17:08 -0300

    -a

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,7 +20,23 @@ function criarSnake(){
     }
 }
 
+document.addEventListener('keydown',update)
+
+function update(event){
+    if(event.keyCode === 37 && direction !== 'right') direction = 'left';
+    if(event.keyCode === 38 && direction !== 'up') direction = 'down';
+    if(event.keyCode === 39 && direction !== 'left') direction = 'right';
+    if(event.keyCode === 40 && direction !== 'down') direction = 'up';
+}
+
+
 function iniciarJogo(){
+
+    if(snake[0].x > 15 * box && direction === 'right') snake[0].x = 0;
+    if(snake[0].x < 0 && direction === 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction === 'up') snake[0].y = 0;
+    if(snake[0].y < 0 && direction === 'down') snake[0].y = 16 * box;
+
     criarBG();
     criarSnake();
 
@@ -42,3 +58,5 @@ function iniciarJogo(){
 }
 
 let jogo = setInterval(iniciarJogo,100)
+
+
